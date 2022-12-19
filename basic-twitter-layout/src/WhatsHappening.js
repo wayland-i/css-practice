12b7358ec8bdfdf0c1ba64e6b776d9d09b3230ae
Ian Iversen <ianiversen1@gmail.com>
2022-12-18 21:47:20 -0500

    cannot edit css of wh profile image

--- basic-twitter-layout/src/WhatsHappening.js
+++ basic-twitter-layout/src/WhatsHappening.js
@@ -3,19 +3,29 @@ import { FaSearchLocation, FaImage, FaVideo, FaVoteYea, FaSmile, FaCalendarCheck
 
 function WhatsHappening() {
   return (
-    <div >
-      <h1>Home</h1>
-      <FaHandSparkles />
-      <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--5BGUGTe6--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/891116/5eb7c338-16d5-4d67-a6f0-4cc7005a51d8.jpeg"/>
-      <input placeholder="what's happening?"></input>
-      <button>Tweet</button>
+    <div className='wh-all'>
       <div>
-        <FaImage />
-        <FaVideo />
-        <FaVoteYea />
-        <FaSmile />
-        <FaCalendarCheck />
-        <FaSearchLocation />
+        <h1>Home</h1>
+        <FaHandSparkles />
+      </div>
+      <div>
+        <img 
+          className='profile'
+          alt='profile'
+          src="https://res.cloudinary.com/practicaldev/image/fetch/s--5BGUGTe6--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/891116/5eb7c338-16d5-4d67-a6f0-4cc7005a51d8.jpeg"
+          />
+        <input placeholder="what's happening?"></input>
+      </div>
+      <div>
+        <button>Tweet</button>
+        <div>
+          <FaImage />
+          <FaVideo />
+          <FaVoteYea />
+          <FaSmile />
+          <FaCalendarCheck />
+          <FaSearchLocation />
+        </div>
       </div>
     </div>
   )
