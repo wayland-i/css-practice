96bdb1ab16577b173845c27a46b05a1a1bd16f7f
Ian Iversen <ianiversen1@gmail.com>
2022-12-19 16:52:41 -0500

    basic twitter layout done

--- basic-twitter-layout/src/App.js
+++ basic-twitter-layout/src/App.js
@@ -6,7 +6,7 @@ import { useState } from 'react';
 
 function App() {
 
-  const [tweets, setTweets] = useState(['tweet', 'tweet', 'tweet'])
+  const [tweets, setTweets] = useState([]);
 
   return (
     <div className='all'>
--- basic-twitter-layout/src/NewsFeed.js
+++ basic-twitter-layout/src/NewsFeed.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import { FaCommentDots, FaRecycle, FaHeart, FaShareSquare } from "react-icons/fa";
 
 function NewsFeed({tweets}) {
   
+  function Tweet({tweet}) {
+    return(
+      <div className='tweets'>
+          <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--5BGUGTe6--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/891116/5eb7c338-16d5-4d67-a6f0-4cc7005a51d8.jpeg"></img>
+          <div>
+            <div className='tweet-header'>
+              <h1>@ian</h1>
+              <h4>-2h</h4>
+            </div>
+            <div>
+              <text id='tweet'>{tweet}</text>
+            </div>
+            <div className='tweet-footer'>
+              <FaCommentDots/>
+              <FaRecycle/>
+              <FaHeart />
+              <FaShareSquare />
+            </div>
+          </div>
+
+      </div>
+    )
+  }
 
   return (
     <div>
-      {tweets.map((tweet) => <li>{tweet}</li>)}
+      {tweets.map((tweet) => <Tweet tweet={tweet}></Tweet>)}
     </div>
   )
 }
