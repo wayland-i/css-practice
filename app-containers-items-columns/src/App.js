905d45fdb0a4bbafb8b0629caaa7a9fbcfde1447
Ian Iversen <ianiversen1@gmail.com>
2022-12-23 23:48:37 -0500

    containers solved

--- app-containers-items-columns/src/App.js
+++ app-containers-items-columns/src/App.js
@@ -1,19 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 import Container from './Container';
 
 function App() {
 
+  const [containerCount, setContainerCount] = useState(1)
+
   const handleAddContainer = () => {
-    const containers = document.getElementsByClassName('Containers')
-    containers.append(<li>hello</li>)
+    // const containers = document.getElementsByClassName('Containers')
+    setContainerCount(containerCount => containerCount + 1);
   }
+
+  const containers = [];
+  for (let i = 0; i < containerCount; i++) {
+    containers.push(<Container />)
+  }
+  
 
   return (
     <div className="App">
       <button onClick={handleAddContainer}>Add Container</button>
-      <ul className='Containers'>
-
-      </ul>
+      {/* <ul className='Containers'>
+        {containers}
+      </ul> */}
+        {containers}
     </div>
   );
 }
--- app-containers-items-columns/src/Container.js
+++ app-containers-items-columns/src/Container.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Container() {
   return (
-    <div>Container</div>
+    <div>hello</div>
   )
 }
 
