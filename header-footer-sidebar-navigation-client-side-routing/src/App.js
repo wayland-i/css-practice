261f364d3005090a3e723ab4833fb3c3c3d26eea
Ian Iversen <ianiversen1@gmail.com>
2022-12-25 14:39:40 -0500

    components made and router installed, links set in app

--- header-footer-sidebar-navigation-client-side-routing/src/App.js
+++ header-footer-sidebar-navigation-client-side-routing/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from "react-router-dom";
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import SideBarNav from './Components/SideBarNav';
+import HomePage from './Components/HomePage';
+import NewsPage from './Components/NewsPage';
+import UserPage from './Components/UserPage';
+import SettingsPage from './Components/SettingsPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <SideBarNav />
+      
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/news">
+          <NewsPage />
+        </Route>
+        <Route path="/user">
+          <UserPage />
+        </Route>
+        <Route path="/settings">
+          <SettingsPage />
+        </Route>
+      </Switch>
+
+      <Footer />
     </div>
   );
 }
---
+++ header-footer-sidebar-navigation-client-side-routing/src/Components/SideBarNav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NavLink, Link } from "react-router-dom";
+
+function SideBarNav() {
+  return (
+    <div>SideBarNav</div>
+  )
+}
+
+export default SideBarNav;
