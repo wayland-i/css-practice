65dfa42b9621b1396d3d734a2b938d04db31c2f5
Ian Iversen <ianiversen1@gmail.com>
2022-12-25 15:48:40 -0500

    finished with header-footer-sidebarnav

--- header-footer-sidebar-navigation-client-side-routing/src/Components/SideBarNav.js
+++ header-footer-sidebar-navigation-client-side-routing/src/Components/SideBarNav.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import { NavLink, Link } from "react-router-dom";
 
+import { AiOutlineHome, AiOutlineUser, AiOutlineSetting } from "react-icons/ai";
+import { BsNewspaper } from "react-icons/bs";
+
 function SideBarNav() {
   return (
-    <div className='nav'>SideBarNav</div>
+    <div className='nav'>
+      <NavLink to="/">
+          <AiOutlineHome />
+      </NavLink>
+      <NavLink to="/news">
+        <BsNewspaper />
+      </NavLink>
+      <NavLink to="/user">
+        <AiOutlineUser />
+      </NavLink>
+      <NavLink to="/settings">
+        <AiOutlineSetting />
+      </NavLink>
+    </div>
   )
 }
 
