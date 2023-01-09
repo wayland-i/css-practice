5cf80e23169a54ff8d1481ffd6a09e8307b4f875
wayland-i <ianiversen1@gmail.com>
2023-01-08 22:57:52 -0500

    feat: another solution to the code sandbox css problem, no styled components

--- replit-homepage/src/App.js
+++ replit-homepage/src/App.js
@@ -1,56 +1,17 @@
-// import './App.css';
-import styled from 'styled-components';
-
-const Red = styled.div`
-  background-color: red;
-  width: 100px;
-  height: 100px;
-`
-const Blue = styled.div`
-  background-color: blue;
-  width: 100px;
-  height: 100px;
-`
-const Purple = styled.div`
-  background-color: purple;
-  width: 100px;
-  height: 100px;
-`
-const Green = styled.div`
-  background-color: green;
-  width: 100px;
-  height: 100px;
-`
-
-const MiniWrap = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin: 0 50% 0 0;
-`
-
-const Wrap = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100%;
-
-`
+import './App.css';
 
 
 function App() {
   return (
-    <Wrap>
+    <div className='App'>
+      <div className='div1'>
+        
+      </div>
 
-      <MiniWrap>
-        <Red className='red' />
-        <Blue className='red' />
-      </MiniWrap>
+      <div className='div2'>
 
-      <MiniWrap>
-        <Purple className='red' />
-        <Green className='red' />
-      </MiniWrap>
-
-    </Wrap>
+      </div>
+    </div>
   );
 }
 
