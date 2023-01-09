06f5f84f2f1af952c45dbe5676775634808f5296
Ian Iversen <ianiversen1@gmail.com>
2023-01-09 14:22:37 -0500

    feat: CodeEditor added to site

--- replit-homepage/src/CodeEditor.js
+++ replit-homepage/src/CodeEditor.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Editor = styled.div`
+    display: flex;
+    justify-content: space-around;
+`
+
+const Main = styled.div`
+    
+    
+`
+
+const Buttons = styled.div`
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 2%;
+`
 
 function CodeEditor() {
   return (
-    <div>
-        
+    <Main>
 
-    </div>
+        <Buttons>
+            
+            <select value='pick a compiler'>
+                <option value="">Pick a compiler...</option>
+                <option value="Node.js">Node.js</option>
+                <option value="Python">Python</option>
+                <option value="C++">C++</option>
+            </select>
+
+            <button style={{ 'backgroundColor': 'green', 'width': '12rem', 'height': '10rem'}}>Run</button>
+
+            <button>Share</button>
+        
+        </Buttons>
+        
+        <Editor>
+            <textarea rows="100" cols="180"></textarea>
+            <textarea rows="100" cols="180"></textarea>
+        </Editor>
+
+    </Main>
   );
 }
 
