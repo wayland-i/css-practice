33a2322a885001390450a8ca299561ea7ab55b01
wayland-i <ianiversen1@gmail.com>
2023-01-08 22:22:57 -0500

    feat: first copying code sandbox problem

---
+++ replit-homepage/src/App.js
@@ -0,0 +1,38 @@
+// import './App.css';
+import styled from 'styled-components';
+
+const Red = styled.div`
+  background-color: red;
+  width: 100px;
+  height: 100px;
+`
+const Red = styled.div`
+  background-color: red;
+  width: 100px;
+  height: 100px;
+`
+const Red = styled.div`
+  background-color: red;
+  width: 100px;
+  height: 100px;
+`
+const Red = styled.div`
+  background-color: red;
+  width: 100px;
+  height: 100px;
+`
+
+
+function App() {
+  return (
+    <div className="App">
+      <Red className='red' />
+      <div className='red' />
+      <div className='red' />
+      <div className='red' />
+
+    </div>
+  );
+}
+
+export default App;
