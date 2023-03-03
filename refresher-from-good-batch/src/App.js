5dbda27ac29c3fe87d41268cdd0d3323818e71d2
wayland-i <ianiversen1@gmail.com>
2023-03-03 18:11:20 -0500

    mock coffee site

--- refresher-from-good-batch/src/App.js
+++ refresher-from-good-batch/src/App.js
@@ -4,7 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header className='header'>
+        <h1>The Coffee Grounds</h1>
+      </header>
+        <nav>
+          <h3>Home</h3>
+          <h3>Client Portal</h3>
+          <h3>Reviews</h3>
+          <h3>Browse Products</h3>
+        </nav>
+        <div className='main-contents'>
+          <h1>Main Contents</h1>
+        </div>
+      <div>
+        <footer className='footer'>
+          <p>The Coffee Grounds copyright 2023</p>
+        </footer>
+      </div>
+
     </div>
   );
 }
