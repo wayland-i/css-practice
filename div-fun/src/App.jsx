26cb17f3447e91544a05bf95bbb9d8577c05a5eb
Ian Iversen <ianiversen1@gmail.com>
2024-02-28 22:59:29 -0500

    style: reviewing div centering

---
+++ div-fun/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+  <>
+  <div id='center'>Hello</div>
+  <div className='container'>
+    <div className='child'>child</div>
+    <div className='child'>child</div>
+    <div className='child'>child</div>
+    <div className='child'>child</div>
+    <div className='child'>child</div>
+    <div className='child'>child</div>
+    <div className='child'>child</div>
+    <div className='child'>child</div>
+    <div className='child'>child</div>
+  </div>
+  </>
+  )
+}
+
+export default App
